62ccc838e7f7e6b64657648dba40bd7bbac54e39
wallzack <vulcheek@gmail.com>
2020-03-19 00:17:29 +0100

    21.3 added dark.hbs

--- server.js
+++ server.js
@@ -1,46 +1,51 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+app.engine('hbs', hbs());
+app.set('view engine', 'hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname + `/views/${name}`));
-  };
-  next();
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.engine('.hbs', hbs({ extname: 'hbs', layoutsDir: './views/layouts', defaultLayout: 'main' }));
+app.set('view engine', '.hbs');
+
+app.get('/style.css', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/style.css'));
+});
+
+app.get('/dark.css', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/dark.css'));
 });
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact.hbs', { layout: 'dark' });
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info');
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
-});
-
-app.get('/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname + '/style.css'));
-});
-
-app.get('/test.png', (req, res) => {
-  res.sendFile(path.join(__dirname + '/test.png'));
+  res.render('history');
 });
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
-});
+})
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
